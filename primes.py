a1a458e1790cdfc474c2be3b880f3c3846052908
Mohamed Abdulrahman <mohamed.abdulrahman@kcl.ac.uk>
2022-10-02 17:16:12 +0100

    my v1 solution

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    count = 0
+    max = 100
+    min = 1
+    for i in range(min,max):
+        for j in range(min,i): 
+            if (i % j == 0)and(j!=1):
+                    break
+            elif i-1==j:
+                list.append(i)
+                count+=1
+                if (count<number_of_primes):
+                    break
+                else:
+                    return list
+
+
+    return list  
